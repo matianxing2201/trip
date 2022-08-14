1ea49a87edbeefcb0dffb7cd3c9cd41faefb05e9
山阴路的夏天。 <450086055@qq.com>
2022-08-14 21:56:17 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+
+</style>
---
+++ src/views/favor/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>facor</div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="less" scoped>
+
+</style>
